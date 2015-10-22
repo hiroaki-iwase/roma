dc3fd359bd4d02572468bf03be1d0f06af31fb4f
hiroaki-iwase <hiroaki.iwase.r@gmail.com>
2015-10-22 16:45:54 +0900

    draft

--- lib/roma/cluster_replication.rb
+++ lib/roma/cluster_replication.rb
@@ -13,12 +13,22 @@ module Roma
         @log = log
         @do_transmit = false
         @stats = Roma::Stats.instance
-        @replica_nodelist = @stats.replica_nodelist
+        addr, port = @stats.replica_nodelist.sample.split(/[:_]/)
+        con = TCPSocket.new(addr, port)
+        con.write("mklhash 0\r\n")
+        @replica_mklhash = con.gets.chomp
+        @log.debug("initial mklhash: [#{@replica_mklhash}]")
+      rescue => e
+        @log.error("Cluster Replication Error: target cluster is NOT booted.")
+        @log.error("#{e}\n#{$@}")
+      ensure
+        con.close
       end
 
       def transmit(cmd)
+        check_mklhash
         @do_transmit = true
-        nid = @replica_nodelist.sample
+        nid = @stats.replica_nodelist.sample
 
         con = Roma::Messaging::ConPool.instance.get_connection(nid)
         raise unless con
@@ -31,8 +41,43 @@ module Roma
         @do_transmit = false
       end
 
+      def check_mklhash
+        nid = @stats.replica_nodelist.sample
+        addr, port = nid.split(/[:_]/)
+        con = TCPSocket.new(addr, port) # [toDO] con pool　利用する
+        con.write("mklhash 0\r\n")
+        current_mklhash = con.gets.chomp
+
+        unless @replica_mklhash == current_mklhash
+          @replica_mklhash = current_mklhash
+          con.write("nodelist\r\n")
+          @stats.replica_nodelist = con.gets.chomp.split("\s")
+          @log.warn("replica cluster's routing was changed.\r\n\tnew replicamklhash: [#{@replica_mklhash}]\r\n\tnew replica nodelist: #{@stats.replica_nodelist}")
+        end
+      rescue Errno::ECONNREFUSED => e
+        @stats.replica_nodelist.delete(nid)
+        @stats.replica_nodelist.each{|nid|
+          addr, port = nid.split(/[:_]/)
+          con = TCPSocket.new(addr, port)
+          con.write("mklhash 0\r\n")
+
+          if con
+            current_mklhash = con.gets.chomp
+            @replica_mklhash = current_mklhash
+            con.write("nodelist\r\n") 
+            @stats.replica_nodelist = con.gets.chomp.split("\s")
+            @log.warn("replica cluster's routing was changed.\r\n\tnew replicamklhash: [#{@replica_mklhash}]\r\n\tnew replica nodelist: #{@stats.replica_nodelist}")
+            break
+          end
+          
+          
+        }
+      rescue => e
+        @log.warn("error class => #{e.class}")
+      end
+
       def close_all
-        @replica_nodelist.each{|nid|
+        @stats.replica_nodelist.each{|nid|
           delete_connection(nid)
         }
       end
